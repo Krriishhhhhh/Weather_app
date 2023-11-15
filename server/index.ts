2ccf39226cb0f5cc810b56a50521d4bb9e87bdaa
Krriishhhhhh <krishgargakio@gmail.com>
2023-11-15 23:08:48 +0530

    f

--- server/index.ts
+++ server/index.ts
@@ -8,6 +8,6 @@ app.use(express.json());
 app.use(cors());
 app.use("/user", userRoutes);
 
-app.listen(5001, () => {
+app.listen(5002, () => {
   console.log("Backend Server is on on port number 5001");
 });
